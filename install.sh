6fac37896ed79daf60985a5fa23626fd13003a57
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-10 11:48:30 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,15 @@
 apt update -y
 apt install git -y
 apt install python3 -y
+apt install php -y
+apt install openssh -y
+pkg update -y
+pkg install git -y
+pkg install python3 -y
+pkg install php -y
+pkg install openssh -y
+
+
 git clone https://github.com/ritheshnayak/RM-phisher
 cd RM-phisher
 git clone https://github.com/EnzoGly/project0/blob/main/programa.py
