e4caa53aaf2ce758351fd7ca4678df67c329bec2
Enzo Bronzon <108149923+EnzoGly@users.noreply.github.com>
2022-09-10 11:42:58 -0300

    Update and rename install.sh to RM-Phisher.sh

---
+++ RM-Phisher.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+apt update -y
+apt install git -y
+apt install python3 -y
+git clone https://github.com/ritheshnayak/RM-phisher
+cd RM-phisher
+git clone https://github.com/EnzoGly/project0/blob/main/programa.py
+chmod +x *
+
+
